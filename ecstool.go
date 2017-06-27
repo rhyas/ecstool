81300953f8eba9f2c630904aa8b54688b59e167e
Jay Eno <jay.eno@mapquest.com>
2017-06-27 11:22:28 -0600

    Updating to support multiple containers on a task

--- ecstool.go
+++ ecstool.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"os"
 
 	"github.com/droundy/goopt"
 
@@ -10,17 +11,19 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ecs"
-	"os"
 )
 
 var cluster = goopt.String([]string{"-c", "--cluster"}, "default", "ECS Cluster to query")
 var region = goopt.String([]string{"-r", "--region"}, "us-east-1", "AWS Region")
+var profile = goopt.String([]string{"-p", "--profile"}, "default", "AWS Profile")
+var containerID = goopt.String([]string{"-i", "--containerID"}, "unknown", "Container ID")
 var show = goopt.Alternatives([]string{"-s", "--show"},
-	[]string{"all", "ServiceTasks", "ManualTasks"},
+	[]string{"all", "ServiceTasks", "ManualTasks", "ContainerInfo"},
 	"Details to print about Cluster Services & Tasks")
 
 var version = "0.1"
 var containerInstanceMap = make(map[string]string)
+var containerInstanceIDMap = make(map[string]string)
 var pageNum = 0
 
 var ecsSvc *ecs.ECS
@@ -28,17 +31,17 @@ var ec2Svc *ec2.EC2
 
 func main() {
 
-	// Setup the Session
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(*region)})
-	if err != nil {
-		panic(err)
-	}
-
 	// Set version
 	goopt.Version = version
 
 	// Parse any args
 	goopt.Parse(nil)
+
+	// Setup the Session
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: aws.String(*region)},
+		Profile: *profile,
+	}))
 
 	// Connect up to ECS
 	ecsSvc = ecs.New(sess)
@@ -50,7 +53,7 @@ func main() {
 	fmt.Println("Using Region: " + *region)
 	fmt.Println("Querying Cluster: " + *cluster)
 
-	buildContainerMap() // Build a map of Container ARN to DNS Names
+	buildContainerMaps() // Build a map of Container ARN to DNS Names
 	switch *show {
 	case "all":
 		listServiceTasks() // List details for Service managed tasks.
@@ -59,21 +62,61 @@ func main() {
 		listServiceTasks()
 	case "ManualTasks":
 		listManualTasks()
+	case "ContainerInfo":
+		findContainerInfo(containerID)
 	default:
 		panic("Unrecognized show option.")
 	}
 }
 
-func buildContainerMap() {
+func die(message string) {
+	fmt.Println(message)
+	os.Exit(255)
+}
+
+func findContainerInfo(id *string) {
+	buildContainerMaps()
+
+	taskInput := &ecs.ListTasksInput{Cluster: aws.String(*cluster)}
+	taskListResult, err := ecsSvc.ListTasks(taskInput)
+	if err != nil {
+		die(err.Error())
+	}
+	if len(taskListResult.TaskArns) >= 1 {
+		oneoffTasksInput := &ecs.DescribeTasksInput{Tasks: taskListResult.TaskArns, Cluster: aws.String(*cluster)}
+		taskDescResult, err := ecsSvc.DescribeTasks(oneoffTasksInput)
+		if err != nil {
+			die(err.Error())
+		}
+		if len(taskDescResult.Tasks) >= 1 {
+			for _, taskInfo := range taskDescResult.Tasks {
+				fmt.Println(taskInfo.Containers[0])
+				/*
+				if taskInfo.StartedBy == nil {
+					fmt.Println("\t\tContainer " +
+						*taskInfo.Containers[0].Name +
+						") running on " +
+						containerInstanceMap[*taskInfo.ContainerInstanceArn] + ":" +
+						strconv.FormatInt(*taskInfo.Containers[0].NetworkBindings[0].HostPort, 10) +
+						" (" + containerInstanceIDMap[*taskInfo.ContainerInstanceArn] + ")")
+				}
+				*/
+			}
+		}
+	}
+	fmt.Println("")
+
+}
+
+func buildContainerMaps() {
 	containerListInput := &ecs.ListContainerInstancesInput{Cluster: aws.String(*cluster)}
 	containerList, err := ecsSvc.ListContainerInstances(containerListInput)
 	if err != nil {
-		panic(err)
+		die(err.Error())
 	}
 
 	if len(containerList.ContainerInstanceArns) == 0 {
-		fmt.Println("No Containers in " + *cluster + " Cluster! (i.e. nothing to run containers on)")
-		os.Exit(255)
+		die("No Containers in " + *cluster + " Cluster! (i.e. nothing to run containers on)")
 	}
 
 	containerDescInput := &ecs.DescribeContainerInstancesInput{
@@ -82,20 +125,22 @@ func buildContainerMap() {
 	}
 	containerDescriptions, err := ecsSvc.DescribeContainerInstances(containerDescInput)
 	if err != nil {
-		panic(err)
+		die(err.Error())
 	}
 
 	for _, desc := range containerDescriptions.ContainerInstances {
 
+		//fmt.Println(desc)
 		input := &ec2.DescribeInstancesInput{
 			InstanceIds: []*string{desc.Ec2InstanceId},
 		}
 
 		instancesResult, err := ec2Svc.DescribeInstances(input)
 		if err != nil {
-			panic(err)
+			die(err.Error())
 		}
 		containerInstanceMap[*desc.ContainerInstanceArn] = *instancesResult.Reservations[0].Instances[0].PrivateDnsName
+		containerInstanceIDMap[*desc.ContainerInstanceArn] = *instancesResult.Reservations[0].Instances[0].InstanceId
 	}
 }
 
@@ -106,7 +151,7 @@ func listServiceTasks() {
 	input := &ecs.ListServicesInput{Cluster: aws.String(*cluster), MaxResults: aws.Int64(10)}
 	res := ecsSvc.ListServicesPages(input, listTasksByService)
 	if res != nil {
-		panic(res)
+		die(res.Error())
 	}
 	fmt.Println("")
 }
@@ -116,7 +161,7 @@ func listTasksByService(page *ecs.ListServicesOutput, lastPage bool) bool {
 	dsInput := &ecs.DescribeServicesInput{Cluster: aws.String(*cluster), Services: page.ServiceArns}
 	servicesRes, err := ecsSvc.DescribeServices(dsInput)
 	if err != nil {
-		panic(err)
+		die(err.Error())
 	}
 	for _, service := range servicesRes.Services {
 		fmt.Println("Service Name: " + *service.ServiceName)
@@ -129,17 +174,36 @@ func listTasksByService(page *ecs.ListServicesOutput, lastPage bool) bool {
 		}
 		taskListResult, err := ecsSvc.ListTasks(taskInput)
 		if err != nil {
-			panic(err)
+			die(err.Error())
 		}
 		if len(taskListResult.TaskArns) >= 1 {
 			fmt.Println("\tThere are " + strconv.Itoa(len(taskListResult.TaskArns)) + " tasks running under this service:")
 			input := &ecs.DescribeTasksInput{Tasks: taskListResult.TaskArns, Cluster: aws.String(*cluster)}
 			taskDescResult, err := ecsSvc.DescribeTasks(input)
 			if err != nil {
-				panic(err)
+				die(err.Error())
 			}
 			for _, taskInfo := range taskDescResult.Tasks {
-				fmt.Println("\t\tContainer " + *taskInfo.Containers[0].Name + " running on " + containerInstanceMap[*taskInfo.ContainerInstanceArn])
+				if (len(taskInfo.Containers) > 1) {
+					fmt.Println("\t\t***This Task has multiple containers!***")
+				}
+				fmt.Println("\t\tTask: " + *taskInfo.TaskDefinitionArn)
+				for i := 0; i < len(taskInfo.Containers); i++ {
+					if (len(taskInfo.Containers[i].NetworkBindings) >= 1) {
+						fmt.Println("\t\t\tContainer " +
+							*taskInfo.Containers[i].Name +
+							" running on " +
+							containerInstanceMap[*taskInfo.ContainerInstanceArn] + ":" +
+							strconv.FormatInt(*taskInfo.Containers[i].NetworkBindings[0].HostPort, 10) +
+							" (" + containerInstanceIDMap[*taskInfo.ContainerInstanceArn] + ")")
+					} else {
+						fmt.Println("\t\t\tContainer " +
+							*taskInfo.Containers[i].Name +
+							" running on " +
+							containerInstanceMap[*taskInfo.ContainerInstanceArn] +
+							" (" + containerInstanceIDMap[*taskInfo.ContainerInstanceArn] + ")")
+					}
+				}
 			}
 			fmt.Println("")
 		}
@@ -159,18 +223,37 @@ func listManualTasks() {
 	taskInput := &ecs.ListTasksInput{Cluster: aws.String(*cluster)}
 	taskListResult, err := ecsSvc.ListTasks(taskInput)
 	if err != nil {
-		panic(err)
+		die(err.Error())
 	}
 	if len(taskListResult.TaskArns) >= 1 {
 		oneoffTasksInput := &ecs.DescribeTasksInput{Tasks: taskListResult.TaskArns, Cluster: aws.String(*cluster)}
 		taskDescResult, err := ecsSvc.DescribeTasks(oneoffTasksInput)
 		if err != nil {
-			panic(err)
+			die(err.Error())
 		}
 		if len(taskDescResult.Tasks) >= 1 {
 			for _, taskInfo := range taskDescResult.Tasks {
 				if taskInfo.StartedBy == nil {
-					fmt.Println("\t\tContainer " + *taskInfo.Containers[0].Name + " running on " + containerInstanceMap[*taskInfo.ContainerInstanceArn])
+					if (len(taskInfo.Containers) > 1) {
+						fmt.Println("\t\t***This Task has multiple containers!***")
+					}
+					fmt.Println("\t\tTask: " + *taskInfo.TaskDefinitionArn)
+					for i := 0; i < len(taskInfo.Containers); i++ {
+						if (len(taskInfo.Containers[i].NetworkBindings) >= 1) {
+							fmt.Println("\t\t\tContainer " +
+								*taskInfo.Containers[i].Name +
+								" running on " +
+								containerInstanceMap[*taskInfo.ContainerInstanceArn] + ":" +
+								strconv.FormatInt(*taskInfo.Containers[i].NetworkBindings[0].HostPort, 10) +
+								" (" + containerInstanceIDMap[*taskInfo.ContainerInstanceArn] + ")")
+						} else {
+							fmt.Println("\t\t\tContainer " +
+								*taskInfo.Containers[i].Name +
+								" running on " +
+								containerInstanceMap[*taskInfo.ContainerInstanceArn] +
+								" (" + containerInstanceIDMap[*taskInfo.ContainerInstanceArn] + ")")
+						}
+					}
 				}
 			}
 		}
